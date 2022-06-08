989487c1c1f92cf96d6c2d0563390b9906785594
AlForuq <muhammad.alforuq@gmail.com>
2022-06-08 17:35:57 +0500

    canvas done

---
+++ lesson.js
@@ -0,0 +1,12 @@
+// console.log('0' - 1)
+
+// console.log
+function name(str) {
+    for (let i = 0; i < str.lenght; i++){
+        if (str[i] =='#') {
+          return  str[i].replace('#', ')')
+        }
+    }
+}
+console.log('#ads##f3fd')
+
